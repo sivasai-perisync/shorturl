749623227db76edd33cdfeef37b13ae8057a3aa0
Avudari sivasai <sivasai@perisync.work>
2025-03-06 16:33:59 +0530

    short url api call,design

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+import {Routes,Route} from "react-router-dom";
+import Home from './Pages';
+
+
+function App() {
+
+
+  return (
+    <>
+      <Routes>
+      <Route path="/" element=<Home /> ></Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,jsx,tsx,js}"],
+  theme: {
+    extend: {
+      screens: {
+        'sm': '300px',
+        'md': '700px',
+        'lg': '1000px',
+        'xl': '1460px',
+      },
+    },
+  },
+  plugins: [],
+}
